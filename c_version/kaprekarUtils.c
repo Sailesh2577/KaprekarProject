f5e6938b5a26b1338c7a4127e492ec1dc0d94030
Sailesh2577 <42528517+Sailesh2577@users.noreply.github.com>
2018-09-07 19:22:18 -0500

    modulous

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -37,7 +37,7 @@ int isKaprekar(int n) {
   long square = n * (long) n;
   //compute the number of digits of the square
   int numDigits = (int) log10(square) + 1;
-  long modulus = 10;
+  long modulous = 10;
   long first, second;
 
   //for each possible "split" of the square...
